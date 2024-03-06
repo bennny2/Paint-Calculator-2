7b929f76f22a808722eb18cd04aa6435e8fd8a66
bennny2 <benhurley01@outlook.com>
2024-03-06 16:55:36 +0000

    Added calculation methods to RoomDimensions

--- PaintCalc2/RoomDimensions.cs
+++ PaintCalc2/RoomDimensions.cs
@@ -12,5 +12,29 @@ namespace PaintCalc2
     public double FloorArea { get; set; }
     public double RoomVolume { get; set; }
     public double PaintNeeded { get; set; }
+
+    //Methods
+
+    public void CalculateFloorArea() {
+        FloorArea = RoomWidth * RoomLength;
+    }
+
+    public void CalculateRoomVolume() {
+        RoomVolume = FloorArea * RoomHeight;
+    }
+
+    public void CalculatePaintNeeded() {
+
+        //Equation to find the surface area of a cuboid
+        PaintNeeded = 2 * ((RoomWidth * RoomLength) + (RoomWidth * RoomHeight) + (RoomLength * RoomHeight));
+
+        //Remove one floorArea worth as the floor likely wont be painted
+        PaintNeeded -= FloorArea;
+
+        //Check whether the Ceiling is being painted
+        if (!CeilingIncluded) {
+            PaintNeeded -= FloorArea;
+        }
+    }
     }
 }
