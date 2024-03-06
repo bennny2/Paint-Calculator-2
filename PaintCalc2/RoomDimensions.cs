c3912f8a35022a2674d69c96b69eafc8f8d97354
bennny2 <benhurley01@outlook.com>
2024-03-06 16:50:44 +0000

    Added properties to RoomDimensions

--- PaintCalc2/RoomDimensions.cs
+++ PaintCalc2/RoomDimensions.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace PaintCalc2
 {
     public class RoomDimensions
     {
-        
+    //Properties
+
+    public double RoomWidth { get; set; }
+    public double RoomHeight { get; set; }
+    public double RoomLength { get; set; }
+
+    public bool CeilingIncluded { get; set; }
+    public double FloorArea { get; set; }
+    public double RoomVolume { get; set; }
+    public double PaintNeeded { get; set; }
     }
 }
